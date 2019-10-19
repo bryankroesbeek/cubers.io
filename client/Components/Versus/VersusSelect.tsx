c74da97075177ccee9a758d97742386564c3c825
Bryan Kroesbeek <bryank.b96@gmail.com>
2019-10-19 17:25:51 +0200

    Create versus search page

--- client/Components/Versus/VersusSelect.tsx
+++ client/Components/Versus/VersusSelect.tsx
@@ -3,18 +3,41 @@ import { SearchableInput } from './SearchableInput'
 
 type VersusSelectProps = {}
 
-type VersusSelectState = {}
+type VersusSelectState = {
+    user1: { username: string } | "none"
+    user2: { username: string } | "none"
+}
 
 export class VersusSelect extends React.Component<VersusSelectProps, VersusSelectState>{
     constructor(props: VersusSelectProps) {
         super(props)
+
+        this.state = {
+            user1: "none",
+            user2: "none"
+        }
+    }
+
+    handleClick() {
+        if (this.state.user1 === "none") return
+        if (this.state.user2 === "none") return
+
+
     }
 
     render() {
         return <div className="versus-search-page">
-            <SearchableInput
-                onNameChosen={(username) => {/* store chosen username */}}
-            />
+            <h2 className="versus-search-title">Competitor Showdown</h2>
+            <div className="versus-search-inputs">
+                <SearchableInput
+                    onNameChosen={username => { console.log(username)}}
+                />
+                <span>vs</span>
+                <SearchableInput
+                    onNameChosen={username => { }}
+                />
+            </div>
+            <button className="versus-search-confirm" onClick={() => { }}>Go!</button>
         </div>
     }
 }
