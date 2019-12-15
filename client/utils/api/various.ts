b97b6a16ac3a1cef42008aea5920d8818b83dfd5
Bryan Kroesbeek <bryank.b96@gmail.com>
2019-12-15 18:07:52 +0100

    Add api call to kinchranks

--- client/utils/api/various.ts
+++ client/utils/api/various.ts
@@ -1,5 +1,6 @@
 import { fetchResources } from './base'
 import { EventRecords, SumOfRanks } from '../types'
+import { KinchRanks } from '../types/kinchRanks'
 
 export function getRecords(event: string, type: "single" | "average"): Promise<EventRecords> {
     return fetchResources<EventRecords>(`/api/records/${event}`)
@@ -11,4 +12,8 @@ export function getSumOfRanks(type: string): Promise<SumOfRanks> {
 
 export function getUsernames(usernameSection: string) {
     return fetchResources<string[]>(`/api/matching-usernames?section=${usernameSection}`)
+}
+
+export function getKinchranks(type: string): Promise<KinchRanks> {
+    return fetchResources(`/api/kinchranks/${type}`)
 }
