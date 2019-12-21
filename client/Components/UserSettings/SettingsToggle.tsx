3d608814c74d0d3a4bd127528a43563be34416e5
Bryan Kroesbeek <bryank.b96@gmail.com>
2019-12-21 15:51:58 +0100

    Transform settings switch into a toggle

--- client/Components/UserSettings/SettingsToggle.tsx
+++ client/Components/UserSettings/SettingsToggle.tsx
@@ -1,39 +1,28 @@
 import * as React from 'react'
 
-// import * as Types from '../../api/types'
-// import * as Helpers from '../../api/helpers/settingsHelper'
-
 type SettingsToggleProps = {
     toggled: boolean
     disabled: boolean
     setSwitch: (value: boolean) => void
 }
 
-type SettingsToggleState = {}
-
-export class SettingsToggle extends React.Component<SettingsToggleProps, SettingsToggleState>{
-    constructor(props: SettingsToggleProps) {
-        super(props)
-
-        this.state = {}
-    }
-
-    switchOff = (e: React.MouseEvent) => {
+export class SettingsToggle extends React.Component<SettingsToggleProps>{
+    toggleSwitch = (e: React.MouseEvent) => {
         e.preventDefault()
-        if (this.props.disabled) return
-        this.props.setSwitch(false)
-    }
-
-    switchOn = (e: React.MouseEvent) => {
-        e.preventDefault()
-        if (this.props.disabled) return
-        this.props.setSwitch(true)
+        if (this.props.disabled) return;
+        this.props.setSwitch(!this.props.toggled)
     }
 
     render() {
-        return <div className="settings-option-wrapper">
-            <button className={`settings-option true ${this.props.toggled ? "toggled" : ""}`} onClick={this.switchOn}>Yes</button>
-            <button className={`settings-option false ${this.props.toggled ? "" : "toggled"}`} onClick={this.switchOff}>No</button>
-        </div>
+        let toggled = this.props.toggled ? "toggled" : ""
+        let disabled = this.props.disabled ? "disabled" : ""
+
+        return <button className={`button-toggle ${disabled}`} onClick={this.toggleSwitch}>
+            <div className={`button-toggle-container ${toggled}`}>
+                <div className={`button-toggle-switch-wrapper ${toggled}`}>
+                    <div className={`button-toggle-switch ${toggled}`}></div>
+                </div>
+            </div>
+        </button>
     }
 }
