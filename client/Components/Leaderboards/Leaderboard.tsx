9dd12e96b54c0ec212de9ca992b631cc27142731
Bryan Kroesbeek <bryank.b96@gmail.com>
2020-01-04 17:02:39 +0100

    Add view logic for overall points table

--- client/Components/Leaderboards/Leaderboard.tsx
+++ client/Components/Leaderboards/Leaderboard.tsx
@@ -4,12 +4,13 @@ import { DispatchProp, connect, MapStateToProps } from 'react-redux'
 
 import { User, LeaderboardEvent } from '../../utils/types'
 import { LeaderboardTable } from './LeaderboardTable'
-import { LeaderboardState, LeaderboardAction } from '../../utils/store/types/leaderboardTypes'
-import { fetchLeaderboardById, setCurrentActiveEvent, updateBlacklist } from '../../utils/store/actions/leaderboardActions'
+import { LeaderboardState, LeaderboardAction, LeaderboardTableRowOverall } from '../../utils/store/types/leaderboardTypes'
+import { fetchLeaderboardById, setCurrentActiveEvent, updateBlacklist, fetchLeaderboardOverall } from '../../utils/store/actions/leaderboardActions'
 import { Store } from '../../utils/store/types/generalTypes'
 import { showListViewPrompt } from '../../utils/store/actions/promptActions'
 import { PromptAction } from '../../utils/store/types/promptTypes'
 import { Header } from '../Header/Header'
+import { Link } from 'react-router-dom'
 
 type RemoteProps = {
     competitionId: number
@@ -47,22 +48,54 @@ export class LeaderboardComponent extends React.Component<LeaderboardsProps>{
                     <img className="tab-events-header-item-image" src={`/static/images/cube-${e.slug}.png`} />
                 </button>
             })}
+            <button
+                className={`tab-events-header-item ${currentEvent.slug === "overall" ? "active" : ""}`}
+                onClick={() => fetchLeaderboardOverall(this.props.dispatch, this.props.competitionId)}
+            /* button */>
+                <i className={`fas fa-globe-europe tab-events-header-item-image`} />
+            </button>
+        </div>
+    }
+
+    renderOverallTable(rows: LeaderboardTableRowOverall[]) {
+        return <div className="leaderboards-event">
+            <div className="leaderboards-event-header">
+
+            </div>
+
+            <div className="leaderboards-event-table">
+                <table className="table-results table table-sm table-striped table-cubersio">
+                    <thead className="thead-dark">
+                        <tr>
+                            <th>#</th>
+                            <th>User</th>
+                            <th>Points</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {rows.map((r, i) => <tr>
+                            <td>{i + 1}</td>
+                            <td><Link to={`/u/${r.user.name}`}>/u/{r.user.name}</Link></td>
+                            <td>{r.points}</td>
+                        </tr>)}
+                    </tbody>
+                </table>
+            </div>
         </div>
     }
 
     render() {
         if (this.props.leaderboard === "loading") return null
-        if (this.props.currentActiveEvent === "none") return null
-        let { leaderboard, currentActiveEvent, dispatch } = this.props
+        if (this.props.currentActiveEvent !== "none") {
+            let currentActiveEvent = this.props.currentActiveEvent
+            let leaderboard = this.props.leaderboard
 
-        return <div className="leaderboards">
-            {this.renderEvents()}
-            <LeaderboardTable
+            var leaderboardTable = <LeaderboardTable
                 user={this.props.user}
                 currentEvent={currentActiveEvent}
-                leaderboard={leaderboard}
+                leaderboard={this.props.leaderboard}
                 showScrambles={() => {
-                    dispatch(showListViewPrompt(
+                    this.props.dispatch(showListViewPrompt(
                         `Scrambles for ${currentActiveEvent.name}`,
                         leaderboard.scrambles
                     ))
@@ -71,6 +104,12 @@ export class LeaderboardComponent extends React.Component<LeaderboardsProps>{
                     this.props.dispatch(updateBlacklist(this.props.dispatch, row, type))
                 }}
             />
+        }
+        if (this.props.overall !== "none") var leaderboardTable = this.renderOverallTable(this.props.overall)
+
+        return <div className="leaderboards">
+            {this.renderEvents()}
+            {leaderboardTable}
         </div>
     }
 }
