64e47396986e2dc36133002692522b7407718c2e
Bryan Kroesbeek <bryank.b96@gmail.com>
2020-01-05 17:04:39 +0100

    Extract info retrieval into separate methods

--- client/utils/store/actions/baseActions.ts
+++ client/utils/store/actions/baseActions.ts
@@ -7,13 +7,19 @@ import { minifySettings } from '../../helpers/settingsHelper'
 import { updateSettings } from '../../api/settings'
 
 export let getBaseInfo = (dispatch: Dispatch<BaseAction>): BaseAction => {
+    getUserInformation(dispatch)
+    getSettingsForUser(dispatch)
+    return { type: "NONE" }
+}
+
+export let getUserInformation = (dispatch: Dispatch<BaseAction>) => {
     getUserInfo()
         .then(res => dispatch({ type: "FETCH_USER_INFO", user: res }))
+}
 
+export let getSettingsForUser = (dispatch: Dispatch<BaseAction>) => {
     getUserSettings()
         .then(res => dispatch({ type: "GET_USER_SETTINGS", settings: res }))
-
-    return { type: "NONE" }
 }
 
 export let submitNewSettings = (dispatch: Dispatch<BaseAction>, newSettings: CleanUserSettings): BaseAction => {
